a6629a2acb914b9b9d12e136e73f427dce29d944
Nicolly <273281@unifio.edu.br>
2026-08-26 01:37:18 +0000

    Criação das tabelas no SQL

--- ecomerce-back/src/main/java/br/edu/unifio/ecomerce_back/entidades/Categoria.java
+++ ecomerce-back/src/main/java/br/edu/unifio/ecomerce_back/entidades/Categoria.java
@@ -1,5 +1,9 @@
 package br.edu.unifio.ecomerce_back.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 // import lombok.AllArgsConstructor;
 // import lombok.NoArgsConstructor;
 //import lombok.Data;
@@ -9,10 +13,15 @@ import lombok.Setter;
 // @NoArgsConstructor // Bloqueia os construtores padrão
 // @AllArgsConstructor // Cria os construtores completos
 // @Data // Cria os Getters, Setters e Contrutores, Equals, Hashcode e toString
+
+@Entity //especifica que essa classe é uma entidade (classe que representa tabela)
 @Getter //Gera os Getters
 @Setter // Gera os Setters
 public class Categoria {
-    private Integer id;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Short id;
+    
     private String nome;
     private String descricao;
 
